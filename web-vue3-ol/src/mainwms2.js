c02fa5bf769046b1aa5348e218d332d100d8326e
zjh <bobo8eye@163.com>
2024-03-15 15:14:04 +0800

    wms服务访问

---
+++ web-vue3-ol/src/mainwms2.js
@@ -0,0 +1,29 @@
+//import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './components/WmsMap2.vue'
+//import App from './components/WmsTest.vue'
+//import App from './App.vue'
+//import router from './router'
+
+import OpenLayersMap from "vue3-openlayers";
+/*import OpenLayersMap, {
+    type Vue3OpenlayersGlobalOptions,
+  } from "vue3-openlayers";*/
+import "vue3-openlayers/styles.css"; // vue3-openlayers version < 1.0.0-*
+
+const app = createApp(App)
+
+app.use(createPinia())
+//app.use(router)
+
+app.use(OpenLayersMap /* options */);
+/*const options: Vue3OpenlayersGlobalOptions = {
+    debug: true,
+  };
+  app.use(OpenLayersMap, options);
+*/
+//app.use(ElementPlus);
+app.mount('#map')
